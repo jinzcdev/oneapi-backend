58d2ec941ea09af181a34d7aee5ada6b8dde769e
Zhichao Jin <jinzcdev@icloud.com>
2023-07-11 10:48:31 +0800

    feat: add random user avatar api
    - add exception handler for AvatarController
    - clean the redundant codes

--- one-api-interface/src/main/java/top/charjin/oneapi/openinterface/OneApiInterfaceApplication.java
+++ one-api-interface/src/main/java/top/charjin/oneapi/openinterface/OneApiInterfaceApplication.java
@@ -3,10 +3,8 @@ package top.charjin.oneapi.openinterface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class OneApiInterfaceApplication {
 
     public static void main(String[] args) {
--- one-api-interface/src/main/java/top/charjin/oneapi/openinterface/controller/AvatarController.java
+++ one-api-interface/src/main/java/top/charjin/oneapi/openinterface/controller/AvatarController.java
@@ -1,8 +1,7 @@
 package top.charjin.oneapi.openinterface.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -12,21 +11,44 @@ import java.net.URL;
  */
 @RestController
 @RequestMapping("/avatar")
+@Slf4j
 public class AvatarController {
 
-    @PostMapping("/random")
-    public String getAvatarUrlByPost() throws Exception {
+    @GetMapping("/cartoon")
+    public String getCartoonAvatarUrl() {
         String avatarUrl = "https://www.loliapi.com/acg/pp/";
-        HttpURLConnection connection = (HttpURLConnection) new URL(avatarUrl).openConnection();
-        connection.setInstanceFollowRedirects(false); // 禁止自动重定向
-        int responseCode = connection.getResponseCode(); // 获取响应码
-        if (responseCode == HttpURLConnection.HTTP_MOVED_TEMP
-                || responseCode == HttpURLConnection.HTTP_MOVED_PERM) {
-            return connection.getHeaderField("Location");
-        } else {
-            return avatarUrl;
+        return getRedirectUrl(avatarUrl);
+    }
+
+    /**
+     * @param gender male or female
+     * @return AvatarUrl
+     */
+    @GetMapping("/user")
+    public String getUserAvatarUrl(@RequestParam String gender) {
+        String avatarUrl = "https://xsgames.co/randomusers/avatar.php?g=" + gender;
+        return getRedirectUrl(avatarUrl);
+    }
+
+    private String getRedirectUrl(String url) {
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setInstanceFollowRedirects(false); // 禁止自动重定向
+            int responseCode = connection.getResponseCode(); // 获取响应码
+            if (responseCode == HttpURLConnection.HTTP_MOVED_TEMP
+                    || responseCode == HttpURLConnection.HTTP_MOVED_PERM) {
+                return connection.getHeaderField("Location");
+            } else {
+                return url;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("请求异常!");
         }
     }
 
-
+    @ExceptionHandler(Exception.class)
+    private String exceptionHandler(Exception e) {
+        log.error(e.getMessage());
+        return e.getMessage();
+    }
 }
