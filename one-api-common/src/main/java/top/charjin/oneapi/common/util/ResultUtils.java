c8a5aefe8d3bc2465413cdb590a6c1a55354a832
Zhichao Jin <jinzcdev@icloud.com>
2023-07-28 22:07:37 +0800

    fix: change the default code of success to `200`

--- one-api-common/src/main/java/top/charjin/oneapi/common/util/ResultUtils.java
+++ one-api-common/src/main/java/top/charjin/oneapi/common/util/ResultUtils.java
@@ -16,7 +16,7 @@ public class ResultUtils {
      * @return
      */
     public static <T> BaseResponse<T> success(T data) {
-        return new BaseResponse<>(0, data, "ok");
+        return new BaseResponse<>(200, data, "ok");
     }
 
     /**
