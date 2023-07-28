ffb54dfa7bc8c682f70d8b71cfc426db8b4f8f41
Zhichao Jin <jinzcdev@icloud.com>
2023-07-28 22:19:22 +0800

    docs: remove related info

--- one-api-backend/src/main/java/top/charjin/oneapi/backend/config/MyBatisPlusConfig.java
+++ one-api-backend/src/main/java/top/charjin/oneapi/backend/config/MyBatisPlusConfig.java
@@ -9,8 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 /**
  * MyBatis Plus 配置
- *
- * @author https://github.com/liyupi
  */
 @Configuration
 @MapperScan("top.charjin.oneapi.backend.mapper")
