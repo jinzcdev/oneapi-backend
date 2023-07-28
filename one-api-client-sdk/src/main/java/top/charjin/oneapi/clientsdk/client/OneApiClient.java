324c3db0a5da25a60f604cac30bcc0752930845c
Zhichao Jin <jinzcdev@icloud.com>
2023-07-28 22:05:48 +0800

    feat(sdk): add the request `generateUserAvatarUrl`

--- one-api-client-sdk/src/main/java/top/charjin/oneapi/clientsdk/client/OneApiClient.java
+++ one-api-client-sdk/src/main/java/top/charjin/oneapi/clientsdk/client/OneApiClient.java
@@ -8,6 +8,8 @@ import top.charjin.oneapi.clientsdk.JsonResponseModel;
 import top.charjin.oneapi.clientsdk.exception.OneAPISDKException;
 import top.charjin.oneapi.clientsdk.models.GenerateCartoonAvatarUrlRequest;
 import top.charjin.oneapi.clientsdk.models.GenerateCartoonAvatarUrlResponse;
+import top.charjin.oneapi.clientsdk.models.GenerateUserAvatarUrlRequest;
+import top.charjin.oneapi.clientsdk.models.GenerateUserAvatarUrlResponse;
 import top.charjin.oneapi.clientsdk.profile.ClientProfile;
 
 import java.lang.reflect.Type;
@@ -37,7 +39,21 @@ public class OneApiClient extends AbstractClient {
         } catch (JsonSyntaxException e) {
             throw new OneAPISDKException("response message: " + rspStr + ".\n Error message: " + e.getMessage());
         }
-        return rsp.response;
+        return rsp.getData();
+    }
+
+    public GenerateUserAvatarUrlResponse generateUserAvatarUrl(GenerateUserAvatarUrlRequest req) throws OneAPISDKException {
+        JsonResponseModel<GenerateUserAvatarUrlResponse> rsp = null;
+        String rspStr = "";
+        try {
+            Type type = new TypeToken<JsonResponseModel<GenerateUserAvatarUrlResponse>>() {
+            }.getType();
+            rspStr = this.internalRequest(req, "/api/avatar/user");
+            rsp = gson.fromJson(rspStr, type);
+        } catch (JsonSyntaxException e) {
+            throw new OneAPISDKException("response message: " + rspStr + ".\n Error message: " + e.getMessage());
+        }
+        return rsp.getData();
     }
 
 
--- one-api-client-sdk/src/main/java/top/charjin/oneapi/clientsdk/models/GenerateUserAvatarUrlRequest.java
+++ one-api-client-sdk/src/main/java/top/charjin/oneapi/clientsdk/models/GenerateUserAvatarUrlRequest.java
@@ -13,9 +13,19 @@ import java.util.HashMap;
  */
 public class GenerateUserAvatarUrlRequest extends AbstractModel {
 
+    public static final String PARAM_MALE = "male";
+    public static final String PARAM_FEMALE = "female";
     @Expose
     @SerializedName("gender")
     private String gender;
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
     @Override
     protected void toMap(HashMap<String, String> map, String prefix) {
